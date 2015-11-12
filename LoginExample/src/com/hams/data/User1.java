5e1e915147cd98e91c53bfebe6312aa63bd14e86
shiveshk05 <shiveshk05@gmail.com>
2015-11-12 15:52:11 +0530

    first commit

---
+++ LoginExample/src/com/hams/data/User1.java
@@ -0,0 +1,37 @@
+package com.hams.data;
+
+
+
+public class User1 {
+	private String user_id;
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	private String user_name;
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	private String password;
+	
+	private java.sql.Timestamp time_stamp ;
+	public java.sql.Timestamp getTime_stamp() {
+		return time_stamp;
+	}
+	public void setTime_stamp(java.sql.Timestamp time_stamp) {
+		this.time_stamp = time_stamp;
+	}
+	
+
+}
